8de83b95752ba5efc5398e58fb4b860c58ee2a2b
Dmitriy Chirkin <demanychirkin@mail.ru>
2023-08-27 23:29:21 +0300

    Добавлен ConsoleHelper.

---
+++ src/ConsoleHelper.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static final BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return bis.readLine();
+    }
+
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text);
+    }
+
+    public static double readDouble() throws IOException {
+        String text = readString();
+        return Double.parseDouble(text);
+    }
+}
